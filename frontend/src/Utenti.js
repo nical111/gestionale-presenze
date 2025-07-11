6bac3cfbd9218088c4b297468d4b24e6b9349173
Alessandro  Nicita <alessandronicita@MacBook-Air-di-Alessandro.local>
2025-07-11 13:32:29 +0200

    Fix: aggiunto rewrite per supportare il routing client-side

--- frontend/src/Utenti.js
+++
@@ -1,7 +0,0 @@
-// src/utenti.js
-
-export const utenti = {
-  Roberto: { password: '1234', ruolo: 'manager' },
-  Ale: { password: '1111', ruolo: 'admin' },
-  Marta: { password: 'Marta', ruolo: 'team' }
-};
